38575fb0c276f3ee8078f87f18dc3d694e715834
Agost Biro <agostbiro@gmail.com>
2024-09-15 22:40:04 +0200

    Add S01_Calculating solutions

--- my_solutions/C02_Basics/S01_Calculating.lean
+++ my_solutions/C02_Basics/S01_Calculating.lean
@@ -7,10 +7,14 @@ example (a b c : ℝ) : a * b * c = b * (a * c) := by
 
 -- Try these.
 example (a b c : ℝ) : c * b * a = b * (a * c) := by
-  sorry
+  rw [mul_comm c b]
+  rw [mul_assoc b c a]
+  rw [mul_comm a c]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [← mul_assoc a b c]
+  rw [mul_comm a b]
+  rw [mul_assoc b a c]
 
 -- An example.
 example (a b c : ℝ) : a * b * c = b * c * a := by
@@ -20,10 +24,15 @@ example (a b c : ℝ) : a * b * c = b * c * a := by
 /- Try doing the first of these without providing any arguments at all,
    and the second with only one argument. -/
 example (a b c : ℝ) : a * (b * c) = b * (c * a) := by
-  sorry
+  rw [← mul_assoc]
+  rw [mul_comm]
+  rw [← mul_assoc]
+  rw [mul_comm]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [← mul_assoc a]
+  rw [mul_comm a]
+  rw [mul_assoc b]
 
 -- Using facts from the local context.
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
@@ -33,10 +42,15 @@ example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c *
   rw [mul_assoc]
 
 example (a b c d e f : ℝ) (h : b * c = e * f) : a * b * c * d = a * e * f * d := by
-  sorry
+  rw [mul_assoc a]
+  rw [h]
+  rw [← mul_assoc a]
 
 example (a b c d : ℝ) (hyp : c = b * a - d) (hyp' : d = a * b) : c = 0 := by
-  sorry
+  rw [hyp]
+  rw [hyp']
+  rw [mul_comm]
+  rw [sub_self]
 
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h', ← mul_assoc, h, mul_assoc]
@@ -96,11 +110,41 @@ end
 section
 variable (a b c d : ℝ)
 
-example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
-  sorry
+example : (a + b) * (c + d) = a * c + a * d + b * c + b * d :=
+  calc
+    _ = (a + b) * (c + d) := by
+      rfl -- reflexivity
+    _ = a * (c + d) + b * (c + d) := by
+      rw [right_distrib]
+    _ = a * c + a * d + b * (c + d) := by
+      rw [left_distrib]
+    _ = a * c + a * d + (b * c + b * d) := by
+      rw [left_distrib]
+    _ = a * c + a * d + b * c + b * d := by
+      rw [← add_assoc]
 
 example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-  sorry
+  calc
+    _ = (a + b) * (a - b) := by
+      rfl
+    _ = a * (a - b) + b * (a - b)  := by
+      rw [right_distrib]
+    _ = a * a - a * b + b * (a - b)  := by
+      rw [mul_sub]
+    _ = a * a - a * b + (b * a - b * b) := by
+      rw [mul_sub]
+    _ = a * a - a * b + b * a - b * b := by
+      rw [add_sub]
+    _ = a ^ 2 - a * b + b * a - b ^ 2 := by
+      rw [pow_two, pow_two]
+    _ = a ^ 2 - (a * b) + (a * b) - b ^ 2 := by
+      rw [mul_comm]
+    _ = (a * b) - (a * b) + a ^ 2 - b ^ 2 := by
+      rw [sub_add_comm (a ^ 2) (a * b) (a * b)]
+    _ = 0 + a ^ 2 - b ^ 2 := by
+      rw [sub_self]
+    _ = a ^ 2 - b ^ 2 := by
+      rw [zero_add]
 
 #check pow_two a
 #check mul_sub a b c
